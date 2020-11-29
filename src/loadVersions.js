71248c1bdb3c56fce7acc2e8898a209380a81797
Zero Bias <zerobias@icloud.com>
2020-11-29 08:55:43 +0300

    remove fs-extra from loadVersions script

--- src/loadVersions.js
+++ src/loadVersions.js
@@ -1,5 +1,5 @@
 const fetch = require('cross-fetch')
-const fs = require('fs-extra')
+const fs = require('fs').promises
 const path = require('path')
 
 async function main() {
@@ -7,24 +7,24 @@ async function main() {
   const pkg = await data.json()
   const versions = []
   const packages = await Promise.all(
-    Object.keys(pkg.versions).map(v =>
+    Object.keys(pkg.versions).map((v) =>
       fetch(`https://unpkg.com/effector@${v}/package.json`)
-        .then(d => d.json())
+        .then((d) => d.json())
         .catch(() => ({})),
     ),
   )
   for (const p of packages) {
     const file =
-      (p.files || []).filter(file => file === 'effector.cjs.js')[0] || ''
+      (p.files || []).filter((file) => file === 'effector.cjs.js')[0] || ''
     if (file.includes('cjs')) {
-      console.log('cjs', p.version)
       versions.push(p.version)
-    } else {
-      console.error('Wrong version', p.version)
     }
   }
   versions.push('master')
-  await fs.outputJSON(path.join(__dirname, 'versions.json'), versions.reverse())
+  await fs.writeFile(
+    path.join(__dirname, 'versions.json'),
+    JSON.stringify(versions.reverse()),
+  )
 }
 
 main()
