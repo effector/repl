fa827cdb8b1313d276355303bfbb06a4b1c601cb
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2021-12-23 15:47:46 +0300

    rename to $counter

--- src/editor/defaultSourceCode.tsx
+++ src/editor/defaultSourceCode.tsx
@@ -3,16 +3,18 @@ const add = createEvent()
 const sub = createEvent()
 const reset = createEvent()
   
-const counter = createStore(0)
- .on(add, (count, num) => count + num)
- .on(sub, (count, num) => count - num)
- .reset(reset)
+const $counter = createStore(0)
   
-counter.watch(n => console.log('counter: ', n))
+$counter.watch(n => console.log('counter: ', n))
 // counter: 0
 add.watch(() => console.log('add'))
 sub.watch(() => console.log('subtract'))
 reset.watch(() => console.log('reset counter'))
+
+$counter
+  .on(add, (count, num) => count + num)
+  .on(sub, (count, num) => count - num)
+  .reset(reset)
   
 add(2)
 // counter: 2
